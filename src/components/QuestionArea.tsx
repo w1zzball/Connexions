bd0126a4b6ac2d1f7b775b676f48e2cba5409180
w1zzball <aaronibbo@gmail.com>
2025-07-17 22:48:29 +0100

    improve setup styling

--- src/components/QuestionArea.tsx
+++ src/components/QuestionArea.tsx
@@ -6,13 +6,8 @@ export default function QuestionArea({ questionIndex }: { questionIndex: number 
     const { numQuestions, questionSets, setQuestionSets } = useGameConfig();
     return (
         <div className="question-area" style={{ backgroundColor: questionSets[questionIndex]?.color }}>
-            <p>Question:</p>
-            <input className='color-picker' type="color" value={questionSets[questionIndex]?.color}
-                onChange={(e) => {
-                    const newSets = [...questionSets];
-                    newSets[questionIndex].color = e.target.value;
-                    setQuestionSets(newSets);
-                }} />
+            {/* <p>Question:</p> */}
+
             <input className='question-input' type="text" placeholder="Question"
                 maxLength={50} autoComplete="off"
                 value={questionSets[questionIndex]?.question}
@@ -22,7 +17,17 @@ export default function QuestionArea({ questionIndex }: { questionIndex: number 
                     setQuestionSets(newSets);
                 }}
             />
-            <p>Answer:</p>
+            <div className="color-row">
+                <i className="fa-solid fa-eye-dropper" style={{ fontSize: '1.2em', color: '#1e1e1e' }}></i>
+                <input className='color-picker' type="color" value={questionSets[questionIndex]?.color}
+                    onChange={(e) => {
+                        const newSets = [...questionSets];
+                        newSets[questionIndex].color = e.target.value;
+                        setQuestionSets(newSets);
+                    }} />
+
+            </div>
+            {/* <p>Answer:</p> */}
             {Array(numQuestions).fill(0).map((_, index) => (
                 <input key={index} className='answer-input' type="text" placeholder={`Answer ${index + 1}`}
                     value={questionSets[questionIndex]?.answers[index]}
