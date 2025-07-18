3214373e2d2e3b3225ff53205ebf4154ae212b8e
w1zzball <aaronibbo@gmail.com>
2025-07-18 19:25:48 +0100

    gh pages attempt

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://w1zzball.github.io/Connexions/',
+  base: '/Connexions/',
   plugins: [react()],
 })
